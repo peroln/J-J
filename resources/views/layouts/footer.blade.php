51d2232eef368ca0e7f125204037057db94e0a4d
Oleh <olehperetiatko@gmail.com>
2024-03-08 17:46:59 +0000

    Separate blade files

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="">
+    <div class="hstack gap-3 p-3">
+        <div class="copyright "><p class="">&copy; 2024 My Web Page. All rights reserved.</p></div>
+        <div class="ms-auto">
+            <address class="">
+                F28YP78 Lios Mor, apt.14, Newport, Co.Mayo Ireland<br>
+                Phone: +353-(87) 647-0733<br>
+                Email: peretjatkool@gmail.com
+            </address>
+        </div>
+    </div>
+</footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,48 +22,12 @@
 <div id="app">
     <div class="container-fluid p-0 position-relative">
         <img src="{{Vite::asset('storage/app/images/main_bg_image.jpeg')}}" class="img-fluid">
-
-        <nav class="navbar navbar-expand-md gradient-custom fixed-top">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="/">
-                    <i alt="Logo" class="bi bi-house-door
-                d-inline-block align-text-top">LOGO</i>
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
-                        data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar"
-                        aria-label="Переключить навигацию">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="offcanvas offcanvas-end canvas-background" tabindex="-1" id="offcanvasDarkNavbar"
-                     aria-labelledby="offcanvasDarkNavbarLabel">
-                    <div class="offcanvas-header">
-                        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"
-                                aria-label="Закрыть"></button>
-                    </div>
-                    <div class="offcanvas-body">
-                        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                            <li class="nav-item p-xl-3 p-xxl-5">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li class="nav-item p-xl-3 p-xxl-5">
-                                <a class="nav-link" href="#">About us</a>
-                            </li>
-                            <li class="nav-item p-xl-3 p-xxl-5">
-                                <a class="nav-link" href="#">Services</a>
-                            </li>
-                            <li class="nav-item p-xl-3 p-xxl-5">
-                                <a class="nav-link " aria-disabled="true">Contact</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </nav>
+        @include("layouts.menu")
         <div class="top-left"><h1 class="p3">When the grass gets growing </br> we get mowing</h1></div>
         <div class="bottom-right"><h1 class="p3">Gardening </br> services</h1></div>
     </div>
     <div class="main-content">
-        <div class="d-flex align-items-start flex-column">
+        <div class="d-flex align-items-start flex-column first-wrap">
             <div class="card first-element-main-content rounded-end-4">
                 <div class="card-body p-md-5">
                     <h1 class="card-title">Why Chose Us</h1>
@@ -73,7 +37,7 @@
                 </div>
             </div>
         </div>
-        <div class="mt-5 d-flex align-items-end flex-column ">
+        <div class=" d-flex align-items-end flex-column ">
             <div class="card  second-element-main-content rounded-start-4">
                 <div class="card-body p-md-5">
                     <h1 class="card-title">What We Do</h1>
@@ -83,26 +47,18 @@
                 </div>
             </div>
         </div>
+        <div class="d-flex align-items-start flex-column third-wrap">
+            <div class="card third-element-main-content rounded-end-4">
+                <div class="card-body p-md-5">
+                    <h1 class="card-title">Get In Touch</h1>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.
+                        With supporting text below as a natural lead-in to additional content.
+                        With supporting text below as a natural lead-in to additional content.</p>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
-<footer class="p-3">
-    <div class="card-body p-md-5">
-        <h1 class="card-title">Get In Touch</h1>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.
-            With supporting text below as a natural lead-in to additional content.
-            With supporting text below as a natural lead-in to additional content.</p>
-    </div>
-    <div class="hstack gap-3 mt-3 p-3">
-        <div class="copyright"><p>&copy; 2024 My Web Page. All rights reserved.</p></div>
-        <div class="ms-auto">
-            <address class="">
-                F28YP78 Lios Mor, apt.14, Newport, Co.Mayo Ireland<br>
-                Phone: +353-(87) 647-0733<br>
-                Email: peretjatkool@gmail.com
-            </address>
-        </div>
-
-    </div>
-</footer>
+@include("layouts.footer")
 </body>
 </html>
