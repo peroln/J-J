e8fab2bc4f592077303f0740111b80aa652530b5
Oleh <olehperetiatko@gmail.com>
2024-03-12 22:39:50 +0000

    Change color font in blocks and background

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,36 +27,39 @@
         <div class="bottom-right"><h1 class="p3">Gardening </br> services</h1></div>
     </div>
     <div class="main-content">
-        <div class="d-flex align-items-start flex-column first-wrap">
-            <div class="card first-element-main-content rounded-end-4">
-                <div class="card-body p-md-5">
-                    <h1 class="card-title">Why Chose Us</h1>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.
-                        With supporting text below as a natural lead-in to additional content.
-                        With supporting text below as a natural lead-in to additional content.</p>
+        <div class="mask">
+            <div class="d-flex align-items-start flex-column first-wrap">
+                <div class="card first-element-main-content rounded-end-4">
+                    <div class="card-body p-md-5">
+                        <h1 class="card-title">Why Chose Us</h1>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.
+                            With supporting text below as a natural lead-in to additional content.
+                            With supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class=" d-flex align-items-end flex-column ">
+                <div class="card  second-element-main-content rounded-start-4">
+                    <div class="card-body p-md-5">
+                        <h1 class="card-title">What We Do</h1>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.
+                            With supporting text below as a natural lead-in to additional content.
+                            With supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex align-items-start flex-column third-wrap">
+                <div class="card third-element-main-content rounded-end-4">
+                    <div class="card-body p-md-5">
+                        <h1 class="card-title">Get In Touch</h1>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.
+                            With supporting text below as a natural lead-in to additional content.
+                            With supporting text below as a natural lead-in to additional content.</p>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class=" d-flex align-items-end flex-column ">
-            <div class="card  second-element-main-content rounded-start-4">
-                <div class="card-body p-md-5">
-                    <h1 class="card-title">What We Do</h1>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.
-                        With supporting text below as a natural lead-in to additional content.
-                        With supporting text below as a natural lead-in to additional content.</p>
-                </div>
-            </div>
-        </div>
-        <div class="d-flex align-items-start flex-column third-wrap">
-            <div class="card third-element-main-content rounded-end-4">
-                <div class="card-body p-md-5">
-                    <h1 class="card-title">Get In Touch</h1>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.
-                        With supporting text below as a natural lead-in to additional content.
-                        With supporting text below as a natural lead-in to additional content.</p>
-                </div>
-            </div>
-        </div>
+
     </div>
 </div>
 @include("layouts.footer")
