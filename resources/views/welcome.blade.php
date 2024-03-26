41fefd15cc5b00c89415e846d282393c098abddd
Oleh <olehperetiatko@gmail.com>
2024-03-26 23:24:57 +0000

    Add 4 block Reviews

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -113,6 +113,33 @@
                     </div>
                 </div>
             </div>
+            <div class="d-flex align-items-end flex-column">
+                <div class="card  second-element-main-content rounded-start-4 mb-0">
+                    <div class="card-body p-md-5">
+                        <div class="accordion" id="accordionPanel4">
+                            <div class="accordion-item">
+                                <h1 class="card-title text-white mb-0" id="panels4-headingOne">
+                                    Reviews
+                                    <button class="accordion-button collapsed text-white pb-0 px-0" type="button" data-bs-toggle="collapse"
+                                            data-bs-target="#panels4-collapseOne"
+                                            aria-expanded="false"
+                                            aria-controls="panels4-collapseOne">
+                                        <p class="text-white">With supporting text below as a natural lead-in to additional content.</p>
+                                    </button>
+                                </h1>
+                                <div id="panels4-collapseOne" class="accordion-collapse collapse" aria-labelledby="panels4-headingOne">
+                                    <p class="accordion-body text-white pt-0 px-0">
+                                        <strong>This is the first item's accordion body.</strong>
+                                        It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element.
+                                        These classes control the overall appearance, as well as the showing and hiding via CSS transitions.
+                                        You can modify any of this with custom CSS or overriding our default variables.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </div>
     </div>
