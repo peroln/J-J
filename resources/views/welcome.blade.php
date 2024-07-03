f0fe8e0b68c70c66c9245166da965f1263e10ff2
Oleh <olehperetiatko@gmail.com>
2024-07-03 22:51:05 +0100

    Add favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <link rel="icon" href="{{url('/images/person-mowing-the-grass.png')}}" type="image/x-icon">
     @include('layouts.base-head')
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
