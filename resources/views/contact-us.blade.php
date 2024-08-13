7a6f7bd97e379ff676474fa3aab7b039ee6745d1
Oleh <olehperetiatko@gmail.com>
2024-08-13 16:13:19 +0100

    Change size of the map

--- resources/views/contact-us.blade.php
+++ resources/views/contact-us.blade.php
@@ -73,16 +73,15 @@
                         </div>
                     </div>
                     <div>
-                        <iframe class="img-thumbnail google-map"
-                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5016.332973278824!2d-9.218921194536335!3d53.85627840620009!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48595c639fad63df%3A0xb95c50c45e6308e1!2z0JHQsNC70LvQuNC90LLQvtGN0YgsIENvLiDQnNCw0LnQviwgRjIzIEYzMDM!5e0!3m2!1sru!2sie!4v1717602672366!5m2!1sru!2sie"
-                                allowfullscreen="yes" loading="lazy"
-                                referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d130090.85292061538!2d-9.364689562946882!3d53.87281390930948!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48595c639fad63df%3A0xb95c50c45e6308e1!2z0JHQsNC70LvQuNC90LLQvtGN0YgsIENvLiDQnNCw0LnQviwgRjIzIEYzMDM!5e0!3m2!1sru!2sie!4v1723561465481!5m2!1sru!2sie"
+                            class="img-thumbnail google-map" allowfullscreen="" loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @include("layouts.footer")
 </body>
 </html>
